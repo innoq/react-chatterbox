b327a72731aace47c0486c9ed8d9a6cec042b96c
Tobias Eckardt <tobias.eckardt@innoq.com>
2017-10-05 18:15:19 +0200

    assignment 3: refactoring Chatterbox into a stateful component
    adding enter chat functionality

--- src/components/Chatterbox.jsx
+++ src/components/Chatterbox.jsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import Title from "./common/Title";
 import Subtitle from "./common/Subtitle";
 
-export default function Chatterbox() {
-    return <span>
-        <Title>Chatterbox</Title>
-        <Subtitle>A super fancy react chat</Subtitle>
-    </span>;
+const NICKNAMES = ["Speedy Gonzales", "Bilbo Beutlin", "Gollum", "Uncle Sam", "William Wallace", "Robert the Bruce"];
+
+export default class Chatterbox extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            nickname: null,
+        };
+
+        this.onNicknameSubmit = this.onNicknameSubmit.bind(this);
+    }
+
+    onNicknameSubmit(event) {
+        event.preventDefault();
+
+        this.setState({nickname: NICKNAMES[Math.floor(Math.random()*(NICKNAMES.length - 1))]});
+    }
+
+    render() {
+        const { nickname } = this.state;
+
+        return <span>
+            <Title>Chatterbox</Title>
+            <Subtitle>A super fancy react chat</Subtitle>
+            {
+                nickname === null ?
+                    <form onSubmit={this.onNicknameSubmit}>
+                        <button type="submit" onClick={this.onNicknameSubmit}>Enter Chat</button>
+                    </form> :
+                    "Your Nickname: " + nickname
+            }
+        </span>;
+    }
 }
