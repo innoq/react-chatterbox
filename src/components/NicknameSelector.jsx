59201f04cdca514d31d35f0852b0d7743c26a703
Tobias Eckardt <tobias.eckardt@innoq.com>
2017-10-06 15:39:06 +0200

    assignment 4: use lifecycle hooks for a timer (member duration)

--- src/components/NicknameSelector.jsx
+++ src/components/NicknameSelector.jsx
@@ -8,6 +8,7 @@ export default class NicknameSelector extends Component {
 
         this.state = {
             nickname: null,
+            enteredDuration: 0
         };
 
         this.onNicknameSubmit = this.onNicknameSubmit.bind(this);
@@ -17,14 +18,31 @@ export default class NicknameSelector extends Component {
         event.preventDefault();
 
         this.setState({nickname: NICKNAMES[Math.floor(Math.random()*(NICKNAMES.length - 1))]});
+        this.enteredTime = Date.now();
+    }
+
+    updateMemberTime() {
+        if(this.state.nickname) {
+            this.setState({
+                enteredDuration: Math.round((Date.now() - this.enteredTime) / 1000)
+            });
+        }
+    }
+
+    componentDidMount() {
+        this.memberTimer = setInterval(() => this.updateMemberTime(), 2000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.memberTimer);
     }
 
     render() {
-        const { nickname } = this.state;
+        const { nickname, enteredDuration } = this.state;
         return nickname === null ?
             <form onSubmit={this.onNicknameSubmit}>
                 <button type="submit" onClick={this.onNicknameSubmit}>Enter Chat</button>
             </form> :
-            "Your Nickname: " + nickname
+            `Your Nickname: ${nickname} (entered ${enteredDuration} seconds ago)`
     }
 }
