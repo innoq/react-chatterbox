bb21db3312125cd34c25fac41af0353593a6d453
Tobias Eckardt <tobias.eckardt@innoq.com>
2017-10-07 21:06:49 +0200

    assignment 7: connection to the backend
    (cherry picked from commit 17f95ae)

--- src/components/NicknameSelector.jsx
+++ src/components/NicknameSelector.jsx
@@ -9,7 +9,10 @@ export default class NicknameSelector extends Component {
         this.state = {
             nickname: null,
             enteredDuration: 0,
-            newNickname: props.proposedNickname
+            newNickname: props.proposedNickname,
+            entering: false,
+            leaving: false,
+            currentUser: null
         };
 
         this.onNicknameSubmit = this.onNicknameSubmit.bind(this);
@@ -20,13 +23,25 @@ export default class NicknameSelector extends Component {
     onNicknameSubmit(event) {
         event.preventDefault();
 
-        this.setState({
-            nickname: this.state.newNickname,
-            enteredDuration: moment().fromNow()
-        });
+        this.setState({ entering: true });
 
-        this.enteredTime = Date.now();
-        this.props.onStateChanged(true);
+        fetch("/api/users", {
+            method: "POST",
+            headers: new Headers({"Content-Type": "application/json"}),
+            body: JSON.stringify({nickname: this.state.newNickname})
+        }).
+            then(response => response.json()).
+            then(newUser => {
+                    this.setState({
+                        nickname: newUser.nickname,
+                        entering: false,
+                        enteredDuration: moment().fromNow(),
+                        currentUser: newUser
+                    });
+                    this.enteredTime = Date.now();
+                    this.props.onStateChanged(true);
+                }
+        );
     }
 
     updateMemberTime() {
@@ -45,12 +60,25 @@ export default class NicknameSelector extends Component {
 
     leaveChat() {
         this.setState({
-            nickname: null,
-            enteredDuration: 0,
-            newNickname: this.props.proposedNickname
+            leaving: true,
         });
 
-        this.props.onStateChanged(false);
+        fetch("api/users/" + this.state.currentUser.id, {
+            method: "DELETE"
+            }).
+            then(response => {
+                this.setState({
+                    nickname: null,
+                    enteredDuration: 0,
+                    newNickname: this.props.proposedNickname,
+                    currentUser: null,
+                    leaving: false
+                });
+
+                this.props.onStateChanged(false);
+            });
+
+
     }
 
     componentDidMount() {
@@ -62,19 +90,34 @@ export default class NicknameSelector extends Component {
     }
 
     render() {
-        const { nickname, enteredDuration, newNickname } = this.state;
+        const { nickname, enteredDuration, newNickname, entering, leaving } = this.state;
 
-        return nickname === null ?
-            <form onSubmit={this.onNicknameSubmit}>
-                <label htmlFor="nickname">Nickname: </label>
-                <input type="text" id="nickname" value={newNickname} onChange={this.updateNewNickname} />
-                <button type="submit" disabled={newNickname === ""}
-                        onClick={this.onNicknameSubmit}>Enter Chat</button>
-            </form> :
-            <span>
-                {`Your Nickname: ${nickname} (entered ${enteredDuration})`}<br />
-                <button onClick={this.leaveChat}>Leave Chat</button>
-            </span>
+        let output;
+
+        if (!entering && !leaving) {
+            if (nickname === null) {
+                output = <form onSubmit={this.onNicknameSubmit}>
+                    <label htmlFor="nickname">Nickname: </label>
+                    <input type="text" id="nickname" value={newNickname} onChange={this.updateNewNickname} />
+                    <button type="submit" disabled={newNickname === ""}
+                            onClick={this.onNicknameSubmit}>Enter Chat</button>
+                </form>
+            } else {
+                output = <span>
+                    {`Your Nickname: ${nickname} (entered ${enteredDuration})`}<br />
+                    <button onClick={this.leaveChat}>Leave Chat</button>
+                </span>
+            }
+        } else {
+            if(entering) {
+                output = "Entering chat ..."
+            } else {
+                output = "Leaving chat ..."
+            }
+        }
+
+        return output;
+
     }
 }
 
