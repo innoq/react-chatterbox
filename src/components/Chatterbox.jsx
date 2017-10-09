21626f48ac971b9979117e8152d273a73db16f8e
Tobias Eckardt <tobias.eckardt@innoq.com>
2017-10-09 09:46:55 +0200

    assignment 9: now with BrowserRouter

--- src/components/Chatterbox.jsx
+++ src/components/Chatterbox.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import ChatView from "./ChatView";
 import UserProfileContainer from "./user-profile/UserProfileContainer";
 
